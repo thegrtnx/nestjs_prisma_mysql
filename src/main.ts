0565b3e04937bdd55229ca38f1545bd2b7386a76
thegrtnx <thegrtnx@duck.com>
2024-04-27 10:55:36 +0100

    👌Update: class-validator added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
+
+  app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+
   const options = new DocumentBuilder()
     .setTitle('Your API Title')
     .setDescription('Your API description')
@@ -15,7 +19,9 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
+
   SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
