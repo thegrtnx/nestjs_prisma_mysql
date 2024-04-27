43807280a43234c17ca1d14a4c9ee6322c2f1fb1
thegrtnx <thegrtnx@duck.com>
2024-04-27 17:47:25 +0100

    👌Update: guarantor

--- src/guarantor/guarantor.controller.ts
+++ src/guarantor/guarantor.controller.ts
@@ -23,17 +23,20 @@ export class GuarantorController {
     return this.guarantorService.create(createGuarantorDto);
   }
 
-  @Get()
+  @Get('')
+  @ApiOperation({ summary: 'Endpoint for getting all guarantors' })
   findAll() {
     return this.guarantorService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Endpoint for getting a guarantor by id' })
   findOne(@Param('id') id: string) {
     return this.guarantorService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Endpoint for updating a guarantor by id' })
   update(
     @Param('id') id: string,
     @Body() updateGuarantorDto: UpdateGuarantorDto,
@@ -42,6 +45,7 @@ export class GuarantorController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Endpoint for deleting a guarantor by id' })
   remove(@Param('id') id: string) {
     return this.guarantorService.remove(id);
   }
