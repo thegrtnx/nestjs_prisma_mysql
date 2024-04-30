1b56ad5fe1cf3ef888ce1a03bbce06e8444b608f
thegrtnx <thegrtnx@duck.com>
2024-04-30 03:09:51 +0100

    👌Update: CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ import { AppModule } from './app/app.module';
 import { GlobalExceptionFilter } from './util/errorHandler';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
 
   app.use(cookieParser());
   app.useGlobalFilters(new GlobalExceptionFilter());
