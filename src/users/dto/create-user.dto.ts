214460ab58d6c81908a7fa496da371e2bfb45621
ayobamy <ayobamyahmed@gmail.com>
2024-05-09 03:17:12 +0100

    (update): modified customers endpoint

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,67 +1,86 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { UserRole } from '.prisma/client';
-
+import { Gender, LoanType, UserRole, cardType } from '.prisma/client';
 import {
   IsEmail,
   IsNotEmpty,
   IsOptional,
   IsString,
   MinLength,
+  IsEnum,
 } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({
-    example: 'ABC XYZ',
-    required: true,
-  })
+  @ApiProperty({ example: 'Doe', required: true })
   @MinLength(3)
   @IsString()
   @IsNotEmpty()
-  name: string;
+  surname: string;
 
-  @ApiProperty({
-    example: '+234901048496',
-    required: true,
-  })
-  @IsString()
+  @ApiProperty({ example: 'John Jonathan', required: true })
   @MinLength(3)
+  @IsString()
   @IsNotEmpty()
-  number: string;
+  otherNames: string;
 
-  @ApiProperty({
-    example: 'abc@xyz.com',
-    required: true,
-  })
+  @ApiProperty({ example: 'SalaryLoan', required: true })
+  @IsEnum(LoanType)
+  @IsNotEmpty()
+  loanType: LoanType;
+
+  @ApiProperty({ example: 'password123', required: false })
+  @IsString()
+  @IsOptional()
+  password: string;
+
+  @ApiProperty({ example: 'Male', required: true })
+  @IsEnum(Gender)
+  @IsNotEmpty()
+  gender: Gender;
+
+  @ApiProperty({ example: 'User', required: false })
+  @IsEnum(UserRole)
+  @IsOptional()
+  role: UserRole;
+
+  @ApiProperty({ example: 'john@example.com', required: true })
   @IsEmail()
   @IsNotEmpty()
   @MinLength(3)
   email: string;
 
-  @ApiProperty({
-    example: null,
-    required: false,
-  })
+  @ApiProperty({ example: '123 Main St', required: false })
   @IsString()
   @IsOptional()
-  password: string;
+  address: string;
 
-  @ApiProperty({ example: 'User', required: false })
-  @MinLength(3)
-  @IsOptional()
-  role: UserRole;
-
-  @ApiProperty({ example: 300, required: true })
+  @ApiProperty({ example: '1000', required: true })
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
   membership_fee: string;
 
-  @ApiProperty({
-    example: '11, qwerty strees, dotcom road',
-    required: true,
-  })
+  @ApiProperty({ example: 'Nigeria', required: false })
   @IsString()
-  @IsNotEmpty()
-  @MinLength(3)
-  address: string;
+  @IsOptional()
+  nationality: string;
+
+  @ApiProperty({ example: 'NIN', required: false })
+  @IsEnum(cardType)
+  @IsOptional()
+  cardType: cardType;
+
+  @ApiProperty({ example: '123 Home St', required: false })
+  @IsString()
+  @IsOptional()
+  homeAddress: string;
+
+  @ApiProperty({ example: '456 Office Rd', required: false })
+  @IsString()
+  @IsOptional()
+  officeAddress: string;
+
+  @ApiProperty({ example: '1234567890', required: false })
+  @IsString()
+  @IsOptional()
+  telephone: string;
 }
